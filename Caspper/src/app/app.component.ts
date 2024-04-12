a2860886a37f9830d6f762714d6881c1637a3e94
Mohammad omari <m.omari@classera.com>
2024-04-13 00:21:30 +0300

    adding the out let

--- Caspper/src/app/app.component.ts
+++ Caspper/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CASPPER';
+  sideBarDefaultWidth: string = '50px'
+  sideBarOpenedWidth: string = '260px'
+  sideBarWidth: string = this.sideBarDefaultWidth
+  isSideBarOpen: boolean = false
+  
+  toggleSideBarOut = () =>{
+    this.sideBarWidth = this.sideBarDefaultWidth
+    this.isSideBarOpen = false
+  }
+  toggleSideBarIn = () =>{
+    this.sideBarWidth = this.sideBarOpenedWidth 
+    this.isSideBarOpen = true
+  }
 }
